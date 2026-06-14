024204b363e4ec1483ce4b506850036ee5cb15e9
alexb <aborod1@wgu.edu>
2026-06-14 09:57:11 -0400

    added play_game function

--- RandomNumberGame.py
+++ RandomNumberGame.py
@@ -12,3 +12,21 @@ def get_guess(min_number, max_number):
         except ValueError:
             print(f"Please enter a valid number.")
 
+def play_game():
+    unknown_number = random.randint(min_number, max_number)
+    guesses = 0
+
+    print("\n The random number is generated. Try to guess it!")
+
+    while True:
+        guess = get_guess(min_number, max_number)
+        guesses += 1
+
+        if guess < unknown_number:
+            print("Higher!")
+        elif guess > unknown_number:
+            print("Lower!")
+        else:
+            print("Correct!")
+            print("It took you {guesses} guesses.")
+            break
